71fb3c922fcd15ea6d6f76f9ec20b62b1542a929
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2022-06-08 22:14:43 +0800

    日常练习

--- 群友/test.c
+++ 群友/test.c
@@ -1,10 +1,10 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <malloc.h>
-//
-//int main() {
-//    while (1) {
-//        int* p = (int*)malloc(1024);
-//    }
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int main() {
+    while (1) {
+        int* p = (int*)malloc(1024);
+    }
+}
